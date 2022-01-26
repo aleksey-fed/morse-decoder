256108ca5391e00ca94163f8a4f5ee411692d5b0
Aleksey Fedorchenko <alekseyfromperm@gmail.com>
2022-01-26 20:38:01 +0500

    init: morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,60 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let array = [];
+    let result = '';
+  
+    i = 0;
+    while (i < expr.length) {
+        j = i + 10;
+        array.push(expr.slice(i, j));
+        i = i + 10;
+    }
+  
+    array.forEach( item => {
+      let currentArr = [];
+  
+      let i = 0;
+      while (i < item.length) {
+        j = i + 2;
+        currentArr.push(item.slice(i, j));
+        i = i + 2;
+      }
+  
+      currentArr.map( (elem, i) => {
+        switch (elem) {
+            case '11': 
+                currentArr[i] = '-';
+                break;
+            case '10': 
+                currentArr[i] = '.';
+                break; 
+            case '00': 
+                currentArr[i] = '';
+                break; 
+            case '**':
+                currentArr[i] = '*';
+                break;
+            }
+        } );
+  
+      let chars = [];
+      chars.push(currentArr.join(''));
+  
+      chars.forEach( item => {
+        if (item === '*****') {
+            result += ' ';
+        }
+        
+        for (let key in MORSE_TABLE) {
+            if (key === item) {
+                result += MORSE_TABLE[key];
+            }
+        }
+      });
+    } )
+  
+    return result;
 }
 
 module.exports = {
